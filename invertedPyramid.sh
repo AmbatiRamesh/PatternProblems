46eed4a32bdbf2a77a0dab84b8c266256e056a4a
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-06-14 12:23:52 +0530

    Added: inverted pyramid

---
+++ invertedPyramid.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+invertedPyramid() {
+    for ((i=$1; i>=1; i--))
+    do
+        for ((j=$1; j>$i; j--))
+        do
+            printf " "
+        done
+        for ((k=1; k<=2*$i-1; k++))
+        do
+            printf "*"
+        done
+        printf "\n"
+    done
+}
+echo "1. Inverted Pyramid"
+
+read -p "Enter the pattern number: " pattern_type
+read -p "Enter the number of rows: " rows
+
+case $pattern_type in
+    1) echo "Selected pattern: Inverted Pyramid"
+       invertedPyramid $rows
+       ;;
+    *) echo "Invalid pattern number. Please try again."
+       ;;
+esac
